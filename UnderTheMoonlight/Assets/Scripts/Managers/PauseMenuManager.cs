0854274d663850f9320e1cfc757a63503c1e487f
Alex Cline <clinealex10@gmail.com>
2020-07-21 10:47:22 -0500

    Worked to reduce scene dependancies for the scenes to work probably.
    - Pause Menu Manager is now a ScriptableObject.
    - Pause Menu is now loaded dynamically into any scene whose name doesn't contain "Menu_"
    - Pause Menu includes how to play and settings. Both are functional. There is a little flash when enabling and disabling these canvases using the Esc key but to be honest, it's fine for now.
    - The Game Manager now knows the first level by build index.
    - There's probably some other stuff too but I forgot ¯\_(ツ)_/¯

--- UnderTheMoonlight/Assets/Scripts/Managers/PauseMenuManager.cs
+++ UnderTheMoonlight/Assets/Scripts/Managers/PauseMenuManager.cs
@@ -1,29 +1,76 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class PauseMenuManager : MonoBehaviour
+
+[Serializable]
+//[CreateAssetMenu(fileName = "New Pause Menu Manager", menuName = "Pause Menu Manager")]
+public class PauseMenuManager : ScriptableObject
 {
-    [SerializeField] private GameObject menuObject = null;
+    private PauseMenuCanvases canvases = null;
 
-    public bool IsPaused => menuObject.activeSelf;
+    public bool IsPaused { get; private set; } = false;
 
-    private void Start()
+    private void OnEnable()
     {
-        SwitchPauseState(false);
+        SceneManager.sceneLoaded += CheckForPauseMenuLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= CheckForPauseMenuLoaded;
+    }
+
+    /// <summary> Checks to see if the pause menu was loaded. </summary>
+    /// <param name="loadedScene"> Scene that is loaded. </param>
+    /// <param name="loadMode"> The mode in which the scene is loaded. </param>
+    private void CheckForPauseMenuLoaded(Scene loadedScene, LoadSceneMode loadMode)
+    {
+        if (loadedScene.name.Contains("Pause") && loadMode == LoadSceneMode.Additive)
+        {
+            canvases = FindObjectOfType<PauseMenuCanvases>();
+            SwitchPauseState(false);
+        }
     }
 
     /// <summary> Swaps the state of the pause menu. </summary>
     public void SwitchPauseState()
     {
-        SwitchPauseState(!menuObject.activeSelf);
+        SwitchPauseState(!IsPaused);
     }
 
     /// <summary> Swaps the state of the pause menu. </summary>
     /// <param name="isActive"> Should the pause menu be active. </param>
     public void SwitchPauseState(bool isActive)
     {
-        menuObject.SetActive(isActive);
-        Time.timeScale = menuObject.activeSelf ? 0f : 1f;
+        IsPaused = isActive;
+
+        canvases.PauseCanvas.enabled = IsPaused;
+        canvases.HowToPlayCanvas.enabled = false;
+        canvases.SettingsCanvas.enabled = false;
+
+        Time.timeScale = IsPaused ? 0f : 1f;
+    }
+
+    /// <summary> 
+    /// If pause, exits from one menu to the previous menu, if any. 
+    /// If upaused, pauses the game. 
+    /// </summary>
+    public void ChangeMenu()
+    {
+        if (canvases.SettingsCanvas.enabled)
+            canvases.SettingsExitButton.onClick.Invoke();
+        else if (canvases.HowToPlayCanvas.enabled)
+            canvases.HowToPlayExitButton.onClick.Invoke();
+        else
+            SwitchPauseState();
+    }
+
+    /// <summary> Method to be called when the Main Menu Button is clicked. </summary>
+    public void MainMenuButtonClicked()
+    {
+        GameManager.Instance.LoadMainMenu();
     }
 }
